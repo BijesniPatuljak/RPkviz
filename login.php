c175ff3552c625f61ca5d9da8a6bc6dd235332cf
bodario <38029761+bodario@users.noreply.github.com>
2020-07-11 01:37:46 +0200

    Dodavanje CSSa
    Dodavanje CSSa

--- login.php
+++ login.php
@@ -15,16 +15,92 @@ require_once __DIR__ . '/model/QuizService.class.php';
 
 session_start();
 ?>
-
-
+<div class="pravokutnik"></div>
+<div class="header"></div>
+<div class="sadrzaj">
 <h2>Dobrodošli u Quizmaster!</h2>
 
 <form action="menu.php" method = "post">
 <p>Upiši ime: <input type="text" name="user_name" ></p>
 
-<p><input type = "submit" value=" Započni! "></input></p>
+<p><input class="button" type = "submit" value=" Započni! "></input></p>
 
 
+</div>
+<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Indie+Flower" />
+
+<style>
+.header{
+padding: 10px;
+font-family: "Indie Flower";
+font-size: 28px;
+color: white;
+position: absolute;
+top: 0;
+left:0;
+height: 120px;
+width:100vw;
+background-color: #d3d3d3;
+
+}
+
+
+.pravokutnik{
+position: fixed;
+top:0;
+left:0;
+bottom: 0;
+width: 40px;
+background-color: #a3a3a3;
+
+
+
+}
+.sadrzaj{
+position:absolute;
+top:50px;
+left: 60px;
+font-family: "Indie Flower";
+font-size: 25px;
+color: #000;}
+
+a{
+font-family: "Indie Flower";
+font-size: 25px;
+color: #000;
+}
+
+a:hover, a:visited{
+opacity:0.7;
+font-family: "Indie Flower";
+font-size: 25px;
+color: #000;
+}
+
+
+
+input[type="text"]
+{
+    font-size:16px;
+    font-family: "Indie Flower";
+    color: #a3a3a3;
+}
+
+
+.button{
+  background-color: #a3a3a3;
+  border: none;
+  color: #000;
+  font-family: "Indie Flower";
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  font-size: 20px;
+}
+.button:hover{
+opacity:0.7;
+
+}</stlye>
 
 </form>
 </body>
